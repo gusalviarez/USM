b21d52b33e32b4dae3d4c63f06694d1366466146
gusalviarez <gusalv2001@gmail.com>
2024-04-02 11:38:38 -0400

    add boolean to printNames

--- labLenguaje/printNames.php
+++
@@ -1,8 +0,0 @@
-    <?php
-    $names = ["Gus", "Cris"];
-    $ages = [25, 30];
-
-    for ($i = 0; $i < count($names); $i++) {
-        echo "$names[$i] is $ages[$i] years old";
-        echo "<br>";
-    }
--- labLenguaje/printNames.py
+++ labLenguaje/printNames.py
@@ -1,5 +1,6 @@
 names = ["Gus", "Cris"]
 ages = [25, 30]
+is_left_handed = [True,False]
 
 for i in range(len(names)):
-  print(f"{names[i]} is {ages[i]} years old")
+  print(f"{names[i]} is {ages[i]} years old and is left handed {is_left_handed[i]}")
