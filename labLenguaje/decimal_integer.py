c523b92b4aefa88ecedbcf38ff0d08e1e013df4e
gusalviarez <gusalv2001@gmail.com>
2024-04-03 14:41:10 -0400

    add decimal_integer

---
+++ labLenguaje/decimal_integer.py
@@ -0,0 +1,6 @@
+value = 24.38
+intpart = value // 1
+decimalpart =value % 1
+
+print(f"parte entera {intpart}")
+print(f"parte decimal {decimalpart}")
--- labLenguaje/printVars.py
+++ labLenguaje/printVars.py
@@ -5,7 +5,6 @@ var_3 = True
 def typeof (var):
             print(var.__class__.__name__)
 
-
 typeof(var_1)
 typeof(var_2)
 typeof(var_3)
